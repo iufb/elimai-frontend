f090216d2819c11b7447986fc8a659886e2e198e
Yerkow <112828364+yerkow@users.noreply.github.com>
2025-01-16 16:47:30 +0500

    Revert "feat: Sentry integration"

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,8 @@
-import { withSentryConfig } from "@sentry/nextjs"
-import createNextIntlPlugin from "next-intl/plugin"
+import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin()
+const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-export default withSentryConfig(withNextIntl(nextConfig), {
-    org: "bedroom-developers",
-    project: "tickets-frontend",
-    widenClientFileUpload: true,
-    reactComponentAnnotation: {
-        enabled: true
-    },
-    tunnelRoute: "/monitoring",
-    hideSourceMaps: true,
-    disableLogger: true,
-    automaticVercelMonitors: true
-})
+export default withNextIntl(nextConfig);
