fb4a64c2ceed4f5334c3f8ba491f5db89cb0e758
iufb <bashirov3ld2@gmail.com>
2025-02-01 00:56:26 +0500

    feat:not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,20 @@
-import { Center } from "@mantine/core";
-
+import { Metadata } from 'next'
+import Link from 'next/link'
+import styles from './not-found.module.css'
+export const metadata: Metadata = {
+    title: "Страница не найдена"
+}
 export default function NotFound() {
     return <html>
         <body>
-            <Center>
-                Не найлено
-            </Center>
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>
+                        Страница не найдена.
+                    </h1>
+                    <Link className={styles.link} href={'/'}>На главную</Link>
+                </div>
+            </div>
         </body>
     </html >
 }
